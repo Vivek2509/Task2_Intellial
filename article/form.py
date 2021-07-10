73e977df3565f7f172460164109bc6225fbd1f3f
vivek <vivek.intellial@gmail.com>
2021-07-10 17:16:56 +0530

    delete old form

--- article/form.py
+++ article/form.py
@@ -1,40 +1,4 @@
-from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
-from .models import Article
-
-
-class PostForm(forms.ModelForm):
-
-    tag = forms.CharField(label='tag', widget=forms.TextInput(attrs={'placeholder': 'Enter tags separete by comma'}),
-                          required=True,)
-
-    class Meta:
-        model = Article
-        fields = ('author', 'title', 'content', 'published', 'tag')
-
-        widgets = {
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'id_author', 'type': 'hidden'}),
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'id': 'summernote', 'name': 'editordata'}),
-            'tag': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'id_tag'}),
-        }
-
-
-class EditForm(forms.ModelForm):
-
-    tag = forms.CharField(label='tag', widget=forms.TextInput(),
-                          required=True,)
-
-    class Meta:
-        model = Article
-        fields = ('title', 'content', 'published', 'tag')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'content': forms.Textarea(attrs={'class': 'form-control', 'id': 'summernote', 'name': 'editordata'}),
-            'tag': forms.TextInput(attrs={'class': 'form-control', 'value': '', 'id': 'id_tag'}),
-        }
 
 
 class CustomUserCreationForm(UserCreationForm):
